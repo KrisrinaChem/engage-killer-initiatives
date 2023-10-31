a8927806caf905237c36286edb108dfcc2989775
Krisrina Chem <140569870+KrisrinaChem@users.noreply.github.com>
2023-10-31 16:01:44 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,54 @@
+if __name__ == "__main__":
+
+
+        print(f"Random Number: {item}")
+import random
+    main()
+    for item in data:
+
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+    data = generate_random_data()
+
+def main():
+        for (int item : data) {
+        int[] data = new int[10];
+        }
+    }
+}
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+            data[i] = (int) (Math.random() * 100) + 1;
+
+public class RandomDataGenerator {
+        }main();
+        console.log("Random Number: " + item);
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+    }
+    return data;
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+
+function generateRandomData() {
+    const data = generateRandomData();
+function main() {
+
+}
+    });
+    data.forEach(item => {
+}
+    public static void main(String[] args) {
+        int[] data = new int[10];
+            System.out.println("Random Number: " + item);
+        }
+public class RandomDataGenerator {
+
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+    }
+        for (int item : data) {
+        for (int i = 0; i < 10; i++) {
